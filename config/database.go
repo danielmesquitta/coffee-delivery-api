c723eebfc09ef71b14e016ca89c95f402ebfc1a7
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-07-12 20:23:55 -0300

    feat: connected to database

---
+++ config/database.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDatabase() {
+	var err error
+
+	dsn := os.Getenv("DATABASE_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+}
