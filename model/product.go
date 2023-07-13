57b0b7375973277e126979fa6559af660335988f
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-07-13 16:14:15 -0300

    feat: products dtos

--- model/product.go
+++ model/product.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	_ "github.com/go-playground/validator/v10"
 	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
@@ -10,6 +11,20 @@ type Product struct {
 	Name        string         `json:"name"`
 	Tags        pq.StringArray `json:"tags" gorm:"type:text[]"`
 	Description string         `json:"description"`
-	Price       uint           `json:"price"`
+	Price       float64        `json:"price"`
 	Image       string         `json:"image"`
+}
+
+type CreateProductDTO struct {
+	Name        string         `json:"name" validate:"required"`
+	Tags        pq.StringArray `json:"tags"`
+	Description string         `json:"description" validate:"required"`
+	Price       float64        `json:"price" validate:"required,numeric,gte=0"`
+}
+
+type UpdateProductDTO struct {
+	Name        string         `json:"name"`
+	Tags        pq.StringArray `json:"tags"`
+	Description string         `json:"description"`
+	Price       float64        `json:"price" validate:"numeric,gte=0"`
 }
